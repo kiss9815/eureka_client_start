3c2cd409eadc9f023f9334678fc6ca6b99a84afd
juntae <juntae@juntae-asus>
2019-04-02 23:40:27 +0900

    첫 커밋

---
+++ eureka_client_start/src/main/java/com/example/demo/EurekaClientStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientStartApplication.class, args);
+	}
+
+}
